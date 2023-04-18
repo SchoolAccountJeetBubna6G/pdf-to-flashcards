c9b5708ad4581768eb22a3bc59482caffd12002c
JayBlur <98525772+SchoolAccountJeetBubna6G@users.noreply.github.com>
2023-04-18 04:45:56 +0000

    Added react router, made templates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from "react";
 import ImageToTextComponent from "./components/ImageToText/ImageToTextComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../src/components/Navbar";
+import PdfToTextComponent from "./components/PdfToText/PdfToTextComponent";
 
 function App() {
   return (
     <>
-      <ImageToTextComponent
-        title="Convert Image to text!"
-        description="Convert your .png or .jpg file to a .txt file with 3 simple clicks!"
-      />
+      <BrowserRouter>
+        <>
+          <Navbar
+            title="Company name"
+            pdfToTextLink="pdf/text"
+            imageToTextLink="img/text"
+          />
+          <Routes>
+            <Route
+              path="/img/text"
+              element={
+                <ImageToTextComponent
+                  title="Convert Image to text!"
+                  description="Convert your .png or .jpg file to a .txt file with 3 simple clicks!"
+                />
+              }
+            />
+
+            <Route
+              path="/pdf/text"
+              element={
+                <ImageToTextComponent
+                  title="Convert PDF to text!"
+                  description="Convert your .pdf file to a .txt file with 3 simple clicks!"
+                />
+              }
+            />
+
+            <Route path="/pdf/text" element={<PdfToTextComponent />} />
+          </Routes>
+        </>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/PdfToText/PdfToTextConverter.js
+++ src/components/PdfToText/PdfToTextConverter.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import FileInput from "../Templates/FileInput";
 
 export default function PdfToTextConverter() {
+  const [file, setFile] = useState(null);
+  const [text, setText] = useState("");
 
   const generateText = () => {
+    var convertapi = require("convertapi")("nFVFHoiunsHOOpg2");
+    convertapi
+      .convert("pdf", { File: "/path/to/my_file.docx" })
+      .then(function (result) {
+        // get converted file url
+        console.log("Converted file url: " + result.file.url);
+      })
+  };
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
-  }
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // do something with the selected file
+    console.log(file);
+    generateText();
+  };
   return (
-    <div>
-      <FileInput
-        generateText = {generateText}
-      />
-    </div>
+    <FileInput
+      generateText={generateText}
+      text={text}
+      file={file}
+      handleSubmit={handleSubmit}
+      handleFileChange={handleFileChange}
+    />
   );
 }
